9cd8344c1884073d646464e5daf511d6a0a3be2a
Medveditskov Yuriy Sergeevich <sparin285@gmail.com>
2022-02-04 20:16:19 +0300

    Add Publishing EF Context

---
+++ src/GalleryOfLuna.Vk.Publishing.EntityFramework/Model/PublishedImage.cs
@@ -0,0 +1,18 @@
+﻿namespace GalleryOfLuna.Vk.Publishing.EntityFramework.Model
+{
+    public class PublishedImage
+    {
+        public PublishedImage(DateTime publishedOn, string provider, string imageId)
+        {
+            Provider = provider;
+            ImageId = imageId;
+            PublishedOn = publishedOn;
+        }
+
+        public Guid Id { get; set; }
+        public DateTime PublishedOn { get; set; }
+
+        public string Provider { get; set; }
+        public string ImageId { get; set; }
+    }
+}
---
+++ src/GalleryOfLuna.Vk.Publishing.EntityFramework/PublishingDbContext.cs
@@ -0,0 +1,15 @@
+﻿using GalleryOfLuna.Vk.Publishing.EntityFramework.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace GalleryOfLuna.Vk.Publishing.EntityFramework
+{
+    public class PublishingDbContext : DbContext
+    {
+        public DbSet<PublishedImage> PublishedImages { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
