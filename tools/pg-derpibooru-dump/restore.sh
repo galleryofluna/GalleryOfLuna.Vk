8b36cdb9656dc4fba0fff9e7217f2031b55409b4
Medveditskov Yuriy Sergeevich <sparin285@gmail.com>
2022-02-04 21:43:51 +0300

    Fix typo in restore.sh

--- tools/pg-derpibooru-dump/restore.sh
+++ tools/pg-derpibooru-dump/restore.sh
@@ -16,6 +16,6 @@ echo "Restoring database"
 pg_restore --username "$POSTGRES_USER" -e -O -d derpibooru dump.pgdump
 
 echo "Calculating Wilson Score"
-pgsql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "derpibooru" -f /scripts/add-wilson-score.sql
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "derpibooru" -f /scripts/add-wilson-score.sql
 
 echo -e "${GREEN}Initialization of derpibooru database completed${NC}"
